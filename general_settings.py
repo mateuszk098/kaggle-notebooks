9fa621040cd9a2b7a42085c315864df4c0175c9f
Mateusz Kowalczyk <mkowalczyk013@gmail.com>
2023-07-14 19:25:16 +0200

    Update.

--- general_settings.py
+++ general_settings.py
@@ -14,6 +14,7 @@ if ON_KAGGLE:
     warnings.filterwarnings("ignore")
 
 import joblib
+import matplotlib.pyplot as plt
 import numpy as np
 import optuna
 import pandas as pd
@@ -21,9 +22,10 @@ import plotly.express as px
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
 import scipy.stats as stats
+import seaborn as sns
 import shap
 from colorama import Fore, Style
-from IPython.core.display import HTML
+from IPython.core.display import HTML, display_html
 from plotly.subplots import make_subplots
 from scipy.cluster.hierarchy import linkage
 from scipy.spatial.distance import squareform
@@ -55,6 +57,7 @@ HEADERS = {
     "props": "font-style: italic; background-color: #010D36; color: #F2F2F0;",
 }
 DF_STYLE = (INDEX_NAMES, HEADERS, TEXT_HIGHLIGHT)
+DF_CMAP = sns.light_palette("#D4D0A9", as_cmap=True)
 
 # Utility functions.
 def download_dataset_from_kaggle(user, dataset, directory):
@@ -75,6 +78,22 @@ def download_competition_from_kaggle(competition):
         Path("data").mkdir(parents=True, exist_ok=True)
         shutil.unpack_archive(competition + ".zip", "data")
         shutil.move(competition + ".zip", "data")
+
+
+def interpolate_color(color1, color2, t):
+    r1, g1, b1 = int(color1[1:3], 16), int(color1[3:5], 16), int(color1[5:7], 16)
+    r2, g2, b2 = int(color2[1:3], 16), int(color2[3:5], 16), int(color2[5:7], 16)
+    r = int(r1 + (r2 - r1) * t)
+    g = int(g1 + (g2 - g1) * t)
+    b = int(b1 + (b2 - b1) * t)
+    return f"#{r:02X}{g:02X}{b:02X}"
+
+
+def get_interpolated_colors(color1, color2, num_colors=2):
+    """Return `num_colors` interpolated beetwen `color1` and `color2`.
+    Arguments need to be HEX."""
+    num_colors = num_colors + 2
+    return [interpolate_color(color1, color2, i / (num_colors - 1)) for i in range(num_colors)]
 
 
 # Html `code` block highlight. Must be included at the end of all imports!
